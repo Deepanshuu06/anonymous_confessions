7011106eaa2270dc4cef059c47905add9786f57f
deepanshu baghel <106313726+Deepanshuu06@users.noreply.github.com>
2024-05-08 23:39:59 +0530

    creating message card

--- src/components/messageCard.tsx
+++ src/components/messageCard.tsx
@@ -1,22 +1,77 @@
-import React from 'react';
-import { Message } from '@/model/User.model';
-import { Button } from '@/components/ui/button';
+'use client';
 
-interface MessageCardProps {
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { Button } from './ui/button';
+import { X } from 'lucide-react';
+import { useToast } from './ui/use-toast';
+import axios from 'axios';
+import { ApiResponse } from '@/types/ApiResponse';
+
+type messageCardProp = {
   message: Message;
   onMessageDelete: (messageId: string) => void;
-}
+};
 
-const MessageCard: React.FC<MessageCardProps> = ({ message, onMessageDelete }) => {
+const messageCard = ({ message, onMessageDelete }: messageCardProp) => {
+  
+  const toast = useToast();
+  const handleDeleteConfirm = async () => {
+    const response = await axios.delete<ApiResponse>(`/api/delete-message/${message._id}`);
+    toast({
+      title: response.data.message,
+    });
+    onMessageDelete(message._id);
+  };
   return (
-    <div className="bg-gray-100 p-4 rounded-md">
-      <div className="flex justify-between items-center">
-        <h3 className="text-lg font-semibold">{JSON.stringify(message)}</h3>
-        <Button onClick={() => onMessageDelete(message._id)}>Delete</Button>
-      </div>
-      <p className="mt-2">{message.content}</p>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button variant="destructive">
+              <X className="w-5 h-5" />
+            </Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete your account and remove
+                your data from our servers.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction onClick={handleDeleteConfirm}>Continue</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent></CardContent>
+    </Card>
   );
 };
 
-export default MessageCard;
+export default messageCard;
