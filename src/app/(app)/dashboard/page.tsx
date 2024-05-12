aceb3d8e5ccfe759e7715a18718c2cc890a69687
deepanshu baghel <106313726+Deepanshuu06@users.noreply.github.com>
2024-05-12 11:37:25 +0530

    smal bugs fixed

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -128,10 +128,11 @@ const Page = () => {
 
   const username = session?.user?.username || '';
 
-  const baseURL = process.env.PROFILE_BASE_URL
-  console.log(baseURL);
   
+  const baseURL = 'https://www.starbionet.com/'
+  console.log(baseURL);
   const profileUrl = `${baseURL}/u/${username}`;
+  
 
   return (
     <div className="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
